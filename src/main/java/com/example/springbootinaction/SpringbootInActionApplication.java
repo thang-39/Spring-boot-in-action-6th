42183c5321750a692b36c5b0b45f4e648c70058c
thang-39 <trantruongminhthang@gmail.com>
2023-03-14 09:09:29 +0700

    1st

---
+++ src/main/java/com/example/springbootinaction/SpringbootInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootinaction/SpringbootInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
